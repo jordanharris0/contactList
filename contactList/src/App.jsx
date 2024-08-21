b0d48682134d58f9cdc3a065e20d9a4e572712e7
jordanharris0 <jordanharris1833@yahoo.com>
2024-08-21 12:12:16 -0500

    created components

--- contactList/src/App.jsx
+++ contactList/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import ContactList from "./components/ContactList";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <ContactList />
+    </>
+  );
 }
 
 export default App;
